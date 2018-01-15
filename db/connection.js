72643508247853b17eaeb0f6c746a02433eabdb3
Lucas Barros <lbam@cin.ufpe.br>
2018-01-15 14:49:58 -0300

    Add rails boilerplate

--- db/connection.js
+++
@@ -1,8 +0,0 @@
-var mysql=require('mysql2/promise');
-var connection=mysql.createPool({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'demo'
-});
- module.exports=connection;
