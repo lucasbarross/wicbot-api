ac63c5da8df4e212d6a5c8c883a26fd72fc8c670
Lucas Barros <lbam@cin.ufpe.br>
2018-01-17 19:11:11 -0300

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  resources :guilds
-  resources :localizations
-  resources :suggestions
-  resources :answers
-  resources :champions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace 'api' do
     namespace 'v1' do
-
+        resources :localizations
+        resources :suggestions
+        resources :answers
+        resources :champions
     end
   end
 end
