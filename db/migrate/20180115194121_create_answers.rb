a4fc9037535480d0e5b13ce8504e6754ae52370c
Lucas Barros <lbam@cin.ufpe.br>
2018-01-17 21:28:45 -0300

    Edit create answers table

--- db/migrate/20180115194121_create_answers.rb
+++ db/migrate/20180115194121_create_answers.rb
@@ -1,7 +1,7 @@
 class CreateAnswers < ActiveRecord::Migration[5.1]
   def change
     create_table :answers do |t|
-      t.references :player, foreign_key: true
+      t.string :player
       t.references :champion, foreign_key: true
       t.boolean :correct
       t.boolean :hinted
