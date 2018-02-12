f664b54b50a699ca39e6a9f2138d8eb7bee24ff9
Lucas Barros <lbam@cin.ufpe.br>
2018-02-12 15:31:03 -0300

    Fix json parent/relation in status response

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -47,8 +47,8 @@ class Api::V1::AnswersController < ApplicationController
   def status
     
     if params[:user_id]
-      @status = Answer.select('player, COUNT(*)').where(correct: true).group('player').where(player: params[:user_id]).first
-      if @status.exists?
+      @status = Answer.select('player, COUNT(*)').where(correct: true).group('player').where(player: params[:user_id]).take
+      if @status
         @remaining = Champion.all.size - @status[:count]
         @total_tries = Answer.select('player, COUNT(*)').group('player').where(player: params[:user_id]).take;
         @total_tries = @total_tries.count
