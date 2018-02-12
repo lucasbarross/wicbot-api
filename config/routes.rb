8f93790098df113c0c83742c06e174e56544a664
Lucas Barros <lbam@cin.ufpe.br>
2018-02-12 01:45:52 -0300

    Fix hint route

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace 'api' do
     namespace 'v1' do 
-        resources :localizations
         get '/localizations/hint', to: "localizations#hint"
+        resources :localizations
         resources :suggestions
         resources :answers
         get '/status', to: 'answers#status'
